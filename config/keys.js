f8d4f6f47b26581b26b163e86067e0bbacc1c95b
Rishabh Jain <45084203+1RishabhJain@users.noreply.github.com>
2021-05-12 09:26:07 -0400

    keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://rishabh:Western23@cluster0.9j6tp.mongodb.net/mern_shopping_list?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://rishabh:<PASSWORD>@cluster0.9j6tp.mongodb.net/mern_shopping_list?retryWrites=true&w=majority'
 }
