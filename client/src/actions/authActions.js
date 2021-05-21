0a6e06f8a24d301cec1bd9df5b996c5dbd6bfc3e
Rishabh Jain <45084203+1RishabhJain@users.noreply.github.com>
2021-05-20 23:09:50 -0400

    React Login & Access Control
    Added authentication of existing users and varying access control based on if the user is logged in or not.

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -18,7 +18,7 @@ export const loadUser = () => (dispatch, getState) => {
 	dispatch({ type: USER_LOADING });
 
 	axios
-		.get("api/auth/user", tokenConfig(getState))
+		.get("/api/auth/user", tokenConfig(getState))
 		.then((res) =>
 			dispatch({
 				type: USER_LOADED,
@@ -61,6 +61,38 @@ export const register =
 				);
 				dispatch({
 					type: REGISTER_FAIL,
+				});
+			});
+	};
+
+// Login User
+export const login =
+	({ email, password }) =>
+	(dispatch) => {
+		// Headers
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		// Request body
+		const body = JSON.stringify({ email, password });
+
+		axios
+			.post("/api/auth", body, config)
+			.then((res) =>
+				dispatch({
+					type: LOGIN_SUCCESS,
+					payload: res.data,
+				})
+			)
+			.catch((err) => {
+				dispatch(
+					returnErrors(err.response.data, err.response.status, "LOGIN_FAIL")
+				);
+				dispatch({
+					type: LOGIN_FAIL,
 				});
 			});
 	};
